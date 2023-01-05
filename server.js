a8a3ec5c1e71e6f43cdeae04b4e22e40f05f9dea
lifesmile-ae <ecommerce@lifesmile.ae>
2023-01-05 11:07:13 +0400

    Updated App Links

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   const userAgent = useragent.parse(req.headers["user-agent"]);
-  res.json({
-    userAgent,
-  });
+  const device = userAgent.os.family.toString();
+
+  switch (device) {
+    case "Android":
+      res.redirect(
+        "https://play.google.com/store/apps/details?id=com.lifesmile.ecommerce"
+      );
+      break;
+    case "iOS":
+      res.redirect("https://apps.apple.com/ua/app/lifesmile-ae/id6444078111");
+      break;
+    default:
+      res.redirect("https://lifesmile.ae/");
+      break;
+  }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
